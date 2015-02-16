d210fafa8503e5e98157ce923a83c8cff9db6072
Wesley Merkel <ooesili@gmail.com>
2015-02-16 12:48:09 -0700

    Add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+  resources :companies do
+    resources :locations
+    resources :products do
+      resources :reviews
+    end
+  end
+  root 'companies#index'
 end
